df86168328f0b571ce0c8cbcdfeb5ee7630c9f72
Ana Laura <analauradias181@gmail.com>
2025-06-17 09:46:52 -0300

    trabalhando com variaveis

---
+++ backend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,38 @@
+nome = "ana"
+idade = 16
+altura = 1.70
+tem_carteira = False
+
+print("Nome:", nome)
+print("idade:", idade)
+print("altura:", altura)
+print("tem carteira?", tem_carteira)
+
+
+
+coelho = "luna"
+idade = 2
+altura = 0.30
+tem_carteira_de_vacina = True
+
+print("coelho:", coelho)
+print("idade:", idade)
+print("altura:", altura)
+print("tem carteira de vacina?", tem_carteira_de_vacina) 
+
+
+
+
+comida = "macarrao"
+validade = 2.35
+peso = 1.25
+tem_glutem = True
+
+print("comida:", comida)
+print("validade:", validade)
+print("peso:", peso)
+print("tem glutem?", tem_glutem) 
+
+
+
+
