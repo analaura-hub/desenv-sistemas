f575782513c16f448ed5311ea788ea67775b05aa
Ana Laura <analauradias181@gmail.com>
2025-06-23 09:05:37 -0300

    trabalhando com condições

---
+++ backend/logicaProgramacao/02_condicionais.py
@@ -0,0 +1,7 @@
+nota =int(input("digite sua nota: "))
+if (nota >=80):
+    print("parabens.")
+elif (nota <80 and nota >60):
+    print("pode melhorar.")
+elif (nota <60):
+    print("recuperaçao.")
