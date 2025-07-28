2a86643f6cf6bb0ab6073f0c5681306d6ad8028c
ana laura <analauradias181@gmail.com>
2025-07-28 08:53:37 -0300

    laços

---
+++ backend/logicaProgramacao/07_ExerciciosLacos.py
@@ -0,0 +1,10 @@
+soma = 0
+    
+for i in range(21):
+    soma += i 
+    
+# soma = soma + i
+print ("soma dos valores:", soma)
+
+
+# 2- pedir ao usuario valores e soma-los enquanto o usuario nao digitar zero
