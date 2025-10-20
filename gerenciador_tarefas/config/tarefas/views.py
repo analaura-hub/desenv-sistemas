7f614ade05bc9e321141dd399f2f7f5fec5b8367
ana laura <analauradias181@gmail.com>
2025-10-20 08:50:25 -0300

    continuando atividades django

---
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Tarefa
+from django.http import HttpResponse
+
+def listar_tarefas(request):
+    tarefas_salvas = Tarefa.objects.all()
+    contexto = {
+        "minhas_tarefas": tarefas_salvas
+    }
+    return render (request, 'tarefas/lista.html', contexto)
+    
