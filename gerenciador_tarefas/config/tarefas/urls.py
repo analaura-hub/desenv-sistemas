0d90900c41c63511b88f2f7e87d4a6ed02847dad
ana laura <analauradias181@gmail.com>
2025-10-21 09:48:29 -0300

    continuando atividades django

--- gerenciador_tarefas/config/tarefas/urls.py
+++ gerenciador_tarefas/config/tarefas/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.listar_tarefas, name='lista_tarefas'),
+
+    path('<int:tarefa_id>/', views.detalhe_tarefa, name='detalhe_tarefa'),
 ]
--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Tarefa
 from django.http import HttpResponse
 
@@ -8,4 +8,7 @@ def listar_tarefas(request):
         "minhas_tarefas": tarefas_salvas
     }
     return render (request, 'tarefas/lista.html', contexto)
-    
+
+def detalhe_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
+    return render(request, 'tarefas/detalhe.html', {'tarefa': tarefa})
